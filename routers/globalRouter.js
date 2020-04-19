c6de8ddde7ac3161d7878560aeefacdde0f10c55
DHKIM <iedcon12@gmail.com>
2020-04-19 17:27:00 +0900

    upto controllers and views w/o real data

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,14 +1,25 @@
 import express from "express";
 import routes from "../routes";
 import { home, search } from "../controllers/videoController";
-import { login, join, logout } from "../controllers/userController";
+import {
+  getJoin,
+  postJoin,
+  logout,
+  postlogin,
+  getLogin
+} from "../controllers/userController";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.join, join);
-globalRouter.get(routes.login, login);
+
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postlogin);
+
 globalRouter.get(routes.logout, logout);
 
 export default globalRouter;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -8,8 +8,9 @@ import {
 
 const userRouter = express.Router();
 
-userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
+// 함수일 경우 Call로 초기화 해줘야 사용 가능
+userRouter.get(routes.userDetail(), userDetail);
 userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
