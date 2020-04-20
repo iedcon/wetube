4abedb50705f36eac41c6d5be0346b01aa49b2ba
DHKIM <iedcon12@gmail.com>
2020-04-20 18:05:56 +0900

    update db info and applying webpack

--- app.js
+++ app.js
@@ -12,12 +12,14 @@ import { localsMiddleware } from "./middlewares";
 const app = express();
 
 // Using the middleware globally below it
+app.use(helmet());
 app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(helmet());
 app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
